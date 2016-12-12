6b7f5e5bf5cf3da75df4198b2873e005a657b2bd
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-12-12 14:42:52 -0800

    add demos

---
+++ HelloWorld/MainWind.cpp
@@ -0,0 +1,11 @@
+#include <QtGui/QApplication> 
+#include <QLabel> 
+
+int main(int argc, char *argv[]) 
+{ 
+    QApplication a(argc, argv); 
+    // QLabel *label = new QLabel("Hello, world!"); 
+    QLabel *label = new QLabel("<h2><font color='red'>Hello</font>, world!<h2>");
+    label->show(); 
+    return a.exec(); 
+}
---
+++ HelloWorld/readme.md
@@ -0,0 +1,4 @@
+Step 1:
+    qmake -project
+Step 2:
+    qmake helloworld.pro
